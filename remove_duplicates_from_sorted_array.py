472064a3d742fb8cd465574fc4e7d38b87eb87fb
Erik Pereira <erikpdn1@gmail.com>
2024-03-31 21:46:17 -0300

    update code

--- remove_duplicates_from_sorted_array.py
+++ remove_duplicates_from_sorted_array.py
@@ -1,13 +1,15 @@
 def remove_duplicate(nums):
+    dictionary = {}
 
     i = 0
-    while len(nums) - 1 > i:
-        if nums[i] == nums[i+1]:
-            nums.pop(i)
-        else:
+    while len(nums) - 1 >= i:
+        if nums[i] not in dictionary:
+            dictionary[nums[i]] = 1
             i += 1
+        else:
+            nums.pop(i)
 
-    return nums
+    return len(nums)
 
 
 nums = [1, 1, 2, 2, 2, 3, 4, 4]
--- remove_element.py
+++ remove_element.py
@@ -1,11 +1,7 @@
 def remove_element(nums, val):
-    i = 0
-    while len(nums) - 1 >= i:
-        if nums[i] == val:
-            nums.pop(i)
-        else:
-            i += 1
-
+    n = nums.count(val)
+    for _ in range(n):
+        nums.remove(val)
     return len(nums)
 
 
@@ -13,3 +9,12 @@ numbers = [3, 2, 2, 3]
 target = 3
 
 print(remove_element(numbers, target))
+
+# best solution
+
+# n = nums.count(val)
+# k = 0
+# for _ in range(n):
+#     nums.remove(val)
+# return len(nums)
+
